16cf8d7df72eac90e44ad63e753ae69ec5d6fc21
Jdolan182 <jordandolan182@gmail.com>
2023-03-12 22:03:34 +0000

    factories and sanctum

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,14 +10,18 @@ class TestController extends Controller
     public function index()
     {
 
+        
         $json = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
 
         return json_encode($json);
     }
 
-    public function post()
+    public function post(Request $request)
     {
 
+        $json = $request->input('data');
+
+        return json_encode($json);
        //
     }
 }
